e89dd3359446e344d588a7be597cefbefef8c0c8
Ryan Wilkie <ryanwilkie2@gmail.com>
2017-10-22 17:30:26 +0100

    Fix admin login for server

--- admin/php/logout.php
+++ admin/php/logout.php
@@ -4,6 +4,6 @@
 	require_once __DIR__ . '/../../php/db.php';
 	session_start();
 	unset($_SESSION['username'], $_SESSION['password']);
-	header("refresh:0, url=http://localhost/NMT-Website/admin/adminLogin.php ");
+	header("refresh:0, url=http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] . "/../../adminLogin.php ");
 	exit();
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 	require_once('php/db.php');
 	session_start();
 ?>
