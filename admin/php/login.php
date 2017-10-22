151ca7dd41780a6fe57a04097cb1b920ff8480cc
Michal <michalbochenek2@gmail.com>
2017-10-22 10:21:41 +0100

    adminLogin.php page added
    + added column isAdmin to users

--- admin/php/login.php
+++ admin/php/login.php
@@ -9,42 +9,41 @@
 
 	global $con;
 
-	if(isset($_POST["submit"])){
+	if(isset($_POST["Login"])){
         if(empty($_POST["login"]) || empty($_POST["password"])){
             $error = "Both fields are required.";
         }
         else if(!empty($_POST["login"]) || !empty($_POST["password"])){
             // Define $username and $password
             $username=$_POST['login'];
-            $password2=$_POST['password'];
+            $password=$_POST['password'];
 
             // To protect from MySQL injection
             $username = stripslashes($username);
-            $password2 = stripslashes($password2);
+            $password = stripslashes($password);
             $username = mysqli_real_escape_string($con, $username);
-            $password2 = mysqli_real_escape_string($con, $password2);
+            $password = mysqli_real_escape_string($con, $password);
 
             //Check for active username in database
             $sql="SELECT * FROM Users WHERE email='$username'";
             $result=mysqli_query($con,$sql);
             $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-            //super admin account for testing purposes
-            if($username=="admin@email.com"&&$password2=="1234"){
+            //admin account
+            if($username==$row['email']&&$password==$row['password']&&$row['isAdmin']==1){
                 $_SESSION['username'] = $username; // Start Session
-                $_SESSION['password'] = $password2;
-                echo "Welcome Admin, redirecting you to the admin panel";
-                header('Location: '.$_SERVER['HTTP_REFERER']);
+                $_SESSION['password'] = $password;
+                header("refresh:0; url=http://localhost/NMT-website/admin/adminIndex.php");
             }
             else{
-                echo "An unknown error has occurred while checking credentials, please contact administration.";
+                echo "Login not recognized.";
                 echo "Redirecting...";
-                header("refresh:6; url=http://localhost/NMT-website/index.php");
+                header("refresh:6; url=http://localhost/NMT-website/adminLogin.php");
             }
         }
         else{
             echo "Login/Password doesn't exist or unknown error has occured.";
             echo "Redirecting...";
-            header("refresh:6; url=http://localhost/NMT-website/index.php");
+            header("refresh:6; url=http://localhost/NMT-website/adminLogin.php");
         }
     } 
 ?>
