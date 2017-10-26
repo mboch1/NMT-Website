7769528c83e5ed7d6dd357b3765a90c9fe77860c
RyanWilkie <ryanwilkie2@gmail.com>
2017-10-26 14:23:20 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -4,53 +4,45 @@
  * Date: 10/10/2017
  * Time: 13:58
  */
-	require_once('db.php');
-	session_set_cookie_params(86400, '/');
+	require_once __DIR__ . '/../../php/db.php';
+	session_set_cookie_params(86400, "/");
 	session_start(); // Starting Session, cookie set to 1 day
-
+	$baseURL = $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 	global $con;
-
-	if(isset($_POST["submit"])){
-
+	if(isset($_POST["Login"])){
         if(empty($_POST["login"]) || empty($_POST["password"])){
             $error = "Both fields are required.";
         }
         else if(!empty($_POST["login"]) || !empty($_POST["password"])){
             // Define $username and $password
             $username=$_POST['login'];
-            $password2=$_POST['password'];
-
+            $password=$_POST['password'];
             // To protect from MySQL injection
             $username = stripslashes($username);
-            $password2 = stripslashes($password2);
+            $password = stripslashes($password);
             $username = mysqli_real_escape_string($con, $username);
-            $password2 = mysqli_real_escape_string($con, $password2);
-
+            $password = mysqli_real_escape_string($con, $password);
             //Check for active username in database
             $sql="SELECT * FROM Users WHERE email='$username'";
             $result=mysqli_query($con,$sql);
             $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-            
-            if(is_array($row)){
-                //temp solution, no password hashing yet:
-                if($row["email"]==$username && $row["password"]==$password2){
-                    $_SESSION['username'] = $username; // Start Session
-                    $_SESSION['password'] = $password2;
-                    $_SESSION['user_id'] = $row["id"];
-                    header("refresh:1, url=http://localhost/NMT-Website/index.php");
-                }
-
-            }
-                else{
-                    echo "An unknown error has occurred while checking credentials, please contact administration.";
-                    echo "Redirecting...";
-                    header("refresh:6; url=http://localhost/NMT-website/index.php");
-                }
+            //admin account
+            if($username==$row['email']&&$password==$row['password']&&$row['isAdmin']==1){
+                $_SESSION['username'] = $username; // Start Session
+                $_SESSION['password'] = $password;
+                $_SESSION['isAdmin'] = 1;
+                header("refresh:0; url=http://" . $baseURL . "/../../adminIndex.php");
             }
             else{
-                echo "Login/Password doesn't exist or unknown error has occured.";
+                echo "Login not recognized.";
                 echo "Redirecting...";
-                header("refresh:6; url=http://localhost/NMT-website/index.php");
+                header("refresh:6; url=http://" . $baseURL . "/../../adminLogin.php");
             }
         }
+        else{
+            echo "Login/Password doesn't exist or unknown error has occured.";
+            echo "Redirecting...";
+            header("refresh:6; url=http://" . $baseURL . "/../../adminLogin.php");
+        }
+    }
 ?>
